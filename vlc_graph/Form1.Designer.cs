0f0ca8fce6cedbc0329e1a9f336ffc9b34082222
AnochaPay <Anocha_ping@hotmail.com>
2024-05-30 15:48:54 +0700

    add

---
+++ vlc_graph/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace vlc_graph {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(736, 398);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button1.Location = new System.Drawing.Point(431, 452);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Update Factor";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox1.Location = new System.Drawing.Point(431, 417);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(317, 29);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "123";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(12, 436);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "M =";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button2.Location = new System.Drawing.Point(631, 452);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 35);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(204, 436);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 37);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "C =";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox2.Location = new System.Drawing.Point(92, 436);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(106, 38);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox3.Location = new System.Drawing.Point(281, 435);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(106, 38);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "11";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 493);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "VLC Graph";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ vlc_graph/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace vlc_graph {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            File.WriteAllText("vlc_graph_result.txt", "PASS\r\n" + textBox2.Text + "\r\n" + textBox3.Text);
+            close_program();
+        }
+
+        private string cal_m = "";
+        private string cal_c = "";
+        private void timer1_Tick(object sender, EventArgs e) {
+            timer1.Enabled = false;
+            double sumx = 0;
+            double sumy = 0;
+            double sumxy = 0;
+            double sumx2 = 0;
+            double xba = 0;
+            double yba = 0;
+            double beta1 = 0;
+            double beta0 = 0;
+            double n = 0;
+            try {
+                string ss = File.ReadAllText("vlc_graph_value_atmega.txt");
+                ss = File.ReadAllText("vlc_graph_value_dmm.txt");
+            } catch { File.WriteAllText("vlc_graph_result.txt", "FAIL\r\nfile err"); }
+            string[] value_atMega = File.ReadAllLines("vlc_graph_value_atmega.txt");
+            string[] value_Dmm = File.ReadAllLines("vlc_graph_value_dmm.txt");
+            File.Delete("vlc_graph_value_atmega.txt");
+            File.Delete("vlc_graph_value_dmm.txt");
+            List<double> x = new List<double>();
+            List<double> y = new List<double>();
+            foreach (string ss in value_atMega) x.Add(Convert.ToDouble(ss));
+            foreach (string ss in value_Dmm) y.Add(Convert.ToDouble(ss));
+
+            foreach (double xa in x) { sumx += xa; }
+            foreach (double ya in y) { sumy += ya; }
+            for (int i = 0; i < x.Count; i++) {
+                sumxy += x[i] * y[i];
+                sumx2 += x[i] * x[i];
+            }
+            n = x.Count;
+            xba = sumx / n;
+            yba = sumy / n;
+            beta1 = (sumxy - (sumx * sumy) / n) / (sumx2 - (n * xba * xba));
+            beta0 = yba - (beta1 * xba);
+            cal_m = beta1.ToString("0.00");
+            cal_c = beta0.ToString("0.00");
+            if (beta0 > 0) textBox1.Text = "Y = " + cal_m + "X + " + cal_c;
+            else textBox1.Text = "Y = " + cal_m + "X - " + Math.Abs(beta0).ToString("0.00");
+            textBox2.Text = cal_m;
+            textBox3.Text = cal_c;
+
+            chart1.Series.Clear();
+            chart1.Series.Add("Data");
+            chart1.Series.Add(textBox1.Text);
+            chart1.Series[0].ChartType = SeriesChartType.Point;
+            chart1.Series[1].ChartType = SeriesChartType.Line;
+
+            for (int i = 0; i < x.Count; i++) {
+                chart1.Series[0].Points.AddXY(x[i], y[i]);
+            }
+            double minX = x.ToList().Min();
+            double maxX = x.ToList().Max();
+            chart1.Series[1].Points.AddXY(minX, beta0 + minX * beta1);
+            chart1.Series[1].Points.AddXY(maxX, beta0 + maxX * beta1);
+            button1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            File.WriteAllText("vlc_graph_result.txt", "FAIL\r\ncancel");
+            close_program();
+        }
+        private void close_program() {
+            this.Close();
+            Application.Exit();
+        }
+    }
+}
